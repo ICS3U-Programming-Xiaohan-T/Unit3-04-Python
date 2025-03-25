ab0940d406dc5387bf2670022f19c69ad26009da
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-03-25 16:50:14 +0000

    integer_updated.py

---
+++ integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Created by: Xiaohan Tian
+# Created on: Mar 25 2025
+# This program is to ask users tp input an integer and identify it.
+
+
+def main():
+    integer = int(input("please input an integer: "))
+
+    if integer == 0:
+        print("{} is just a zero".format(integer))
+
+    elif integer > 0:
+        print("{} is a positive integer".format(integer))
+
+    elif integer < 0:
+        print("{} is a negative integer".format(integer))
+
+    print("")
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
